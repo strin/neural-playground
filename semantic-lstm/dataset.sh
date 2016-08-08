d42bb046e70c3fca445c281e51b818fe16fa5974
Tianlin Shi <tianlins@stanford.edu>
2016-08-08 15:02:51 -0700

    change dataset to official one in emnlp2015 paper

--- semantic-lstm/dataset.sh
+++ semantic-lstm/dataset.sh
@@ -1,5 +1,6 @@
 # this script pulls data from dropbox link.
-TARGET_DIR='data/laptop'
+TARGET_DIR='data/dact'
 mkdir -p $TARGET_DIR
-wget https://www.dropbox.com/sh/5kh1sggoi76huba/AACqyb0hD8OSZZGng-jPjQdja?dl=1 -O data/laptop.zip
-unzip data/laptop.zip -d TARGET_DIR
+wget https://www.dropbox.com/sh/qxjiwszlz7t1hhu/AAABqeiCv9wo-UOhtJkYZRENa?dl=1 -O data/dact.zip
+unzip data/dact -d $TARGET_DIR
+rm -rf data/dact.zip
