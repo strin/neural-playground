565761bbd6f470379906f05bcade8a434809466c
Tianlin Shi <tianlins@stanford.edu>
2016-08-09 16:54:18 -0700

    add build vocab

--- semantic-lstm/README.md
+++ semantic-lstm/README.md
@@ -16,6 +16,35 @@ A dataset is (dialogue act, utterance) pairs.
 Implementation of semantically conditioned LSTM.
 
 ![](https://www.dropbox.com/s/fozxom37fizhg4e/Screenshot%202016-08-09%2010.34.00.png?dl=1)
+
+### Using pretrained wordvec
+
+Reference:
+[GloVe: Global Vectors for Word Representation](http://www-nlp.stanford.edu/pubs/glove.pdf)
+
+Download pretrained wordvec models from the [github page](https://github.com/stanfordnlp/GloVe).
+
+### Delexicalize
+
+To allow better generalization across different slot values, we first de-lexicalize the values in the input sentence, for example.
+
+```
+{'__type__': 'hello'}
+hello , welcome to the parlance dialogue system ? you can ask for restaurant -s by area , price range or food type . how may i help you ?
+
+{'price_range': 'moderate', '__type__': 'inform', 'name': "'trattoria contadina'"}
+<name> is a nice restaurant in the moderate price range .
+
+{'price_range': 'moderate', '__type__': 'inform', 'name': "'alamo square seafood grill'", 'area': "'friendship village'"}
+<name> is a nice restaurant in the area of <area> and it is in the moderate price range .
+
+{'__type__': 'inform', 'name': "'alamo square seafood grill'", 'address': "'803 fillmore street'"}
+the address for <name> is <address> .
+```
+
+**Discussion:** 1-hot vector representation vs. generalizable representation.
+
+
 ### Minor Todos
 
 - [ ] Init for the DA embedding layer.
--- semantic-lstm/train.py
+++ semantic-lstm/train.py
@@ -20,7 +20,7 @@ def parse_dact(raw_da):
     da = {}
     try:
         result = re.findall(r'(.*)\((.*)\)', raw_da)[0]
-        da['type'] = result[0]
+        da['__type__'] = result[0]
         for raw_field in result[1].split(';'):
             if not raw_field:
                 continue
@@ -84,10 +84,6 @@ def build(da_vocab, word_vocab, max_senlen,
     # bag-of-words averaging.
     model_da.add(Lambda(lambda emb: K.sum(emb, axis=1), output_shape=lambda input_shape: (input_shape[0], input_shape[2])))
     model_da.add(RepeatVector(max_senlen))
-    # word embedding layer.
-    model_word = Sequential()
-    model_word.add(Embedding(len(word_vocab), input_length=max_senlen, output_dim=WORD_EMBED_DIM))
-    model_word.add(Reshape((max_senlen, WORD_EMBED_DIM)))
     # sc-lstm layer.
     model = Sequential()
     model.add(Merge([model_word, model_da], mode='concat', concat_axis=2))
@@ -104,6 +100,34 @@ def build(da_vocab, word_vocab, max_senlen,
                        np.zeros((32, 10), dtype=np.int64)])
 
 
+def build_vocab(data, output_file=None):
+    '''
+    will delexicalize based on dialect act.
+    '''
+    vocab = {}
+    log = open('log/delexicalize_%s' % DOMAIN, 'w')
+    for d in data:
+        sen = d['ref']
+        # delexicalize.
+        print>>log, d['dact']
+        for token, value in d['dact'].items():
+            if token in ['name', 'type', 'price', 'phone', 'count',
+                         'address', 'postcode', 'area', 'near']:
+                value = value.replace('\'', '')
+                sen = sen.replace(value, '<' + token + '>')
+        print>>log, sen, '\n'
+        words = sen.replace('\n', '').strip().split(' ')
+        for word in words:
+            if not word:
+                continue
+            if word not in vocab:
+                vocab[word] = len(vocab)
+    log.close()
+    if not output_file:
+        with open(output_file, 'w') as f:
+            for word in vocab:
+                f.write(word + '\n')
+    return vocab
 
 
 
@@ -113,6 +137,7 @@ if __name__ == '__main__':
     for d in data:
         d['raw_dact'] = str(d['dact'])
         d['dact'] = parse_dact(d['raw_dact'])
-    (data_encoded, da_vocab) = prepare_dact(data)
-    word_vocab = {i:i for i in range(100)}
-    build(da_vocab, word_vocab, max_senlen=10)
+    #(data_encoded, da_vocab) = prepare_dact(data)
+    #word_vocab = {i:i for i in range(100)}
+    #build(da_vocab, word_vocab, max_senlen=10)
+    build_vocab(data, 'vocab_sfxrestaurant.txt')
